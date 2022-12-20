19ccda97dd089c2243b6f1a8d4d64d822e28aadf
Gurvir <boparaigurvir@gmail.com>
2022-12-19 21:55:06 -0500

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,24 +1,49 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import useDebounce from "../hooks/useDebounce";
+import useGetCountry from "../hooks/useGetCountry";
 
 export default function SearchBar() {
-    const [search, setSearch] = useState(false)
+    const search = useRef<null | HTMLInputElement>(null);
+    const [countryName, setCountryName] = useState("");
+    const { country } = useGetCountry(countryName);
 
+    return (
+        <div>
+            <div className=" bg-white w-[584px] min-h-[184px] rounded-3xl flex justify-center items-center shadow-sp flex-col">
+                <div className="space-x-[3rem] p-3">
+                    <input
+                        type="text"
+                        placeholder="Enter a country name"
+                        className="border-b border-black focus:outline-none w-[18rem] font-light"
+                        ref={search}
+                    />
+                    <button
+                        className="w-[124px] h-[49px] rounded-3xl bg-[#A7C7E7] hover:bg-[#4e9ceb] duration-300 ease-in-out "
+                        onClick={handleSearch}
+                    >
+                        Search
+                    </button>
 
-  return (
-    <div>
-        <div className=" bg-white w-[584px] min-h-[184px] rounded-3xl flex justify-center items-center shadow-sp flex-col">
-            <div className='space-x-[3rem] p-3'>
-                <input type="text" placeholder='Enter a country name' className='border-b border-black focus:outline-none w-[18rem] font-light' />
-                <button className="w-[124px] h-[49px] rounded-3xl bg-[#A7C7E7] hover:bg-[#4e9ceb] duration-300 ease-in-out " onClick={() => setSearch(true)}  >Search</button>
-            </div>
-            <div className='flex flex-col mt-5'>
-                { search ? <h1>hello</h1> : ""}
+                </div>
+                <div className="flex flex-col mt-5">
+                    {country ? <div className="w-[584px] p-4">
+                        <div className="flex flex-col items-center">
+                            <img src={country?.flags?.svg} alt="" className="w-[350px] " />
+                            <h1 className="uppercase font-bold text-[24px] my-5">{country.name.official}</h1>
+                        </div>
+                        <p> <b>Common Name: </b> {country.name.common} </p>
+                        <p> <b>Capital: </b> {country.capital} </p>
+                        <p> <b>Continent: </b> {country.continents} </p>
+                        <p> <b>Population: </b> {country.population} </p>
 
+                    </div> : ""}
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
 
+    function handleSearch() {
+        if (search && search.current) setCountryName(search.current?.value);
+    }
+}
